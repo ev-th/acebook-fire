468a49bc16486a6b0090849b118c82f390a07487
Mergim <mergimberisha0@gmail.com>
2023-06-13 11:34:14 +0100

    add navbar links

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,14 +1,19 @@
 import './Navbar.css';
 import React from "react";
+import {Link, useNavigate} from "react-router-dom";
 
 const Navbar = () => {
+    const logout = () => {
+        window.localStorage.removeItem("token")
+        useNavigate('/login')
+      }
     return(
         <header data-cy="navbar" className="navbar">
         <h1>Acebook</h1>
         <div className="navbar-list">
-            <a>My Page</a>
-            <a>Posts</a>
-            <a>Logout</a>
+            <Link to="/"> <a>My Page</a> </Link>
+            <Link to="/posts"> <a>Posts</a> </Link>
+            <a onClick={ logout }>Logout</a>
         </div>
     </header>
     )
