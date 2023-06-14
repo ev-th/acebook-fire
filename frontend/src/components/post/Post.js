829789a13f0f773a133e5cbae8a9f9e1764e74c0
JRSkates <skatesproduction@gmail.com>
2023-06-14 16:21:33 +0100

    Comments displaying frontend, hide/show ticket button, new comment form and submit

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CommentList from '../commentList/commentList';
 import './Post.css';
 
 
 const Post = ({post}) => {
+const [showComments, setShowComments] = useState(false);
+
+const toggleComments = () => {
+  setShowComments(!showComments);
+}
 
 const commentsArr = [{comment: "1st Comment"}, {comment: "2nd Comment"}, {comment: "3rd Comment"}]
 
-  return(
-    <div className="post" data-cy="post" key={ post._id }>
+return (
+  <div className="post" data-cy="post" key={post._id}>
+    <div>
+      <p className="name">{post.firstName + " " + post.lastName}</p>
+      <p>{post.content}</p>
+    </div>
 
-      <div>
-        <p className="name">{post.firstName +" "+ post.lastName}</p> 
-        <p>{ post.content }</p>
-      </div>
-      <div id='feed' role="feed">
-         { console.log(commentsArr)}
-          {
-            commentsArr.slice().reverse().map((comment) => {
-              return <CommentList comment={ comment }/>
-            })
-          }
+    <div className="comments-toggle">
+      <button onClick={toggleComments}>
+        {showComments ? "Hide Comments" : "Show Comments"}
+      </button>
+    </div>
+  
+  {showComments && (
+    <div className="comments" data-cy="comments" key={post._id}>
+
+        <div className="comment-list">
+          {commentsArr.slice().reverse().map((comment) => {
+            return <CommentList comment={comment} />;
+          })}
         </div>
-        <form> 
-          <input placeholder="comment" id="comment" type='text' />
-          <input id='submit' type="submit" value="Submit" />
-        </form>
-      </div>
-  )
-}
+
+        <form className="comment-form">
+        <input placeholder="comment" id="comment" type="text" />
+        <input id="submit" type="submit" value="Submit" />
+      </form>
+    </div>
+  )}
+  </div>
+)};
 
 export default Post;
