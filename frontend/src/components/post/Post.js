15064bb8ad23451ab5e4f7a8d04a0775e04c48c4
marco belahouane <nfk18@hotmail.co.uk>
2023-06-14 14:03:33 +0100

    quick add

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CommentList from '../commentList/commentList';
 import './Post.css';
 
 
 const Post = ({post}) => {
+  
   return(
     <div className="post" data-cy="post" key={ post._id }>
 
@@ -10,6 +12,14 @@ const Post = ({post}) => {
         <p className="name">{post.firstName +" "+ post.lastName}</p> 
         <p>{ post.content }</p>
       </div>
+      <div id='feed' role="feed">
+          {
+            post.commentArray.slice().reverse().map((comment) => {
+              return <commentList comment={ comment }/>
+            })
+          }
+        </div>
+      
       </div>
   )
 }
