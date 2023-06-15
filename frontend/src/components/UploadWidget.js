d8c6defd006d37bff4694ff98a627f1f1bdfd6c7
Verity <veritywong90@gmail.com>
2023-06-15 14:20:00 +0100

    url saved from image upload result

--- frontend/src/components/UploadWidget.js
+++ frontend/src/components/UploadWidget.js
@@ -1,22 +1,30 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const UploadWidget = () => {
  const cloudinaryRef = useRef();
  const widgetRef = useRef();
+ const [imageURL, setImageURL] = useState("");
+
  useEffect(() => {
     cloudinaryRef.current = window.cloudinary;
     widgetRef.current = cloudinaryRef.current.createUploadWidget({
         cloudName: 'dzdwjdv7d',
         uploadPreset: 'cdnf3xhm'
     }, function(error, result) {
-        console.log(result);
+        setImageURL(result.info.secure_url);
+        console.log(result.info)
     })
-
  }, [])
+
+ const openWidget = () => {
+    widgetRef.current.open();
+  };
+
 return (
-    <button onClick={() => widgetRef.current.open()}>
-      Upload
-    </button>
+    <div>
+    <button onClick={openWidget}>Upload Image</button>
+          {imageURL && <img src={imageURL} alt="" />}
+    </div>
 )
 }
 
