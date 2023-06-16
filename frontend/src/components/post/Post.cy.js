132b10fa81b8287fdc8a3b10eefb4089caf3487a
Evan Thomas <evanthomas0292@gmail.com>
2023-06-16 12:15:04 +0100

    Fix test for post by mounting in a router component

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -1,8 +1,10 @@
 import Post from './Post'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 describe("Post", () => {
   it('renders a post with a message', () => {
-    cy.mount(<Post post={{_id: 1, firstName: "Ted", lastName: "Lasso", newPost: "Hello, world"}} />);
+    cy.mount(<Router><Post post={{_id: 1, firstName: "Ted", lastName: "Lasso", newPost: "Hello, world"}} /></Router>);
     cy.get('[data-cy="post"]').should('contain.text', "Hello, world")
     cy.get('[data-cy="post"]').should('contain.text', "Ted Lasso")
   })
