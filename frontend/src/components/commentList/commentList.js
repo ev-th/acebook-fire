42f2e41f32df0628c735b61fd7e4b1d15de5ce51
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-16 14:05:41 +0100

    added css

---
+++ frontend/src/components/commentList/commentList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './commentList.css';
+
+
+  const CommentList = ({comment}) => {
+    return(
+      <div className="comment" data-cy="comment" key={ comment._id }>
+        <div>
+          { <p className="comment"> {comment}</p> }
+        </div>
+        </div>
+    )
+  }
+
+export default CommentList;
